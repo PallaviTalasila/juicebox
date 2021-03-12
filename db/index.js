a756516b5510a6092d3422883ee36db21f47caa4
Pallavi <pallavi01261984@gmail.com>
2021-03-11 21:31:14 -0500

    fix

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 const { Client } = require("pg"); // imports the pg module
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  connectionString:
+    process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev",
 });
 
 /**
